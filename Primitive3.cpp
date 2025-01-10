c909b3bdecbdaf7b1bc0f6b1ed006d03e2efe961
veda-aarushi <121644311+veda-aarushi@users.noreply.github.com>
2025-01-10 14:44:34 -0800

    Update Primitive3.cpp

--- Primitive3.cpp
+++ Primitive3.cpp
@@ -2,9 +2,25 @@
 #include <cmath>
 using namespace std;
 
+int myRoundingFunction(double example) {
+    if (example >= 0) {
+    //if the value is positive, then check if the decimal of the value is greater than or lesser then 0.5
+     return (example - floor(example) >= 0.5);
+    } 
+    else {
+        return (example - floor(example) >= -0.5);
+    }
+
+}
+
+
 int main()
 {
     //These are the test cases that are written in the assignment
-    double examples[] = {0.4, 0.6, -0.4, -0.6};
+    double example = 0.4;
 
+    int result = myRoundingFunction(example);
+     std::cout << "result: " << result << std::endl;
+
+    return 0;
 }
