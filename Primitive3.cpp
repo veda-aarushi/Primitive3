112c8ba649f1b3e2084b89f1de482b67b02f3e04
veda-aarushi <121644311+veda-aarushi@users.noreply.github.com>
2025-01-10 14:06:03 -0800

    Add files via upload

---
+++ Primitive3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    //These are the test cases that are written in the assignment
+    double examples[] = {0.4, 0.6, -0.4, -0.6};
+
+}
